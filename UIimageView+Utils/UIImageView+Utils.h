a9bf0c67a0c36c245a27976e07824a8ad33abd9d
koichi yamamoto <koichi@dividual.jp>
2014-09-11 15:34:34 +0900

    a

--- UIimageView+Utils/UIImageView+Utils.h
+++ UIimageView+Utils/UIImageView+Utils.h
@@ -13,4 +13,7 @@
 /// ブラーをかける
 -(void)applyBlurWithRaduis:(NSInteger)radius tintColor:(UIColor *)tintColor saturationDeltaFactor:(CGFloat)saturationDeltaFactor animated:(BOOL)animated;
 
+/// アニメーション付きで画像をセット
+-(void)setImage:(UIImage *)image animated:(BOOL)animated;
+
 @end
--- UIimageView+Utils/UIImageView+Utils.m
+++ UIimageView+Utils/UIImageView+Utils.m
@@ -14,15 +14,22 @@
 /// ブラーをかける
 -(void)applyBlurWithRaduis:(NSInteger)radius tintColor:(UIColor *)tintColor saturationDeltaFactor:(CGFloat)saturationDeltaFactor animated:(BOOL)animated{
 	[self.image imageByApplyingOptimizedBlurWithRadius:radius tintColor:tintColor saturationDeltaFactor:saturationDeltaFactor queue:nil completion:^(UIImage *result_img) {
-		self.image = result_img;
-		if( animated ){
-			CATransition *transition = [CATransition animation];
-			transition.duration = 0.25;
-			transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-			transition.type = kCATransitionFade;
-			[self.layer addAnimation:transition forKey:nil];
-		}
+		[self setImage:result_img animated:animated];
 	}];
 }
+
+
+/// アニメーション付きで画像をセット
+-(void)setImage:(UIImage *)image animated:(BOOL)animated{
+	[self setImage:image];
+	if( animated ){
+		CATransition *transition = [CATransition animation];
+		transition.duration = 0.25;
+		transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+		transition.type = kCATransitionFade;
+		[self.layer addAnimation:transition forKey:nil];
+	}
+}
+
 
 @end
